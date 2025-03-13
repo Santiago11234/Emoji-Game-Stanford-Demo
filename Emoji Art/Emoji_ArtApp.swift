d180d2571b25fa20b28f044218f4095afca7e7d1
Santiago <santiagogeb@gmail.com>
2025-03-13 13:11:52 -0500

    stanford lecture 10

--- Emoji Art/Emoji_ArtApp.swift
+++ Emoji Art/Emoji_ArtApp.swift
@@ -10,23 +10,12 @@ import SwiftData
 
 @main
 struct Emoji_ArtApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    
+    @StateObject var defaultDocument = EmojiArtDocument()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiArtDocumentView(document: defaultDocument)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
